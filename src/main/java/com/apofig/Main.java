b08f801d4a0fe43f9bc3215969ccb23ed81424cf
Oleksandr Baglai <apofig@gmail.com>
2013-07-16 21:26:29 +0300

    Извлекаем ноту До из javax.sound.midi.*

---
+++ src/main/java/com/apofig/Main.java
@@ -0,0 +1,31 @@
+package com.apofig;
+
+import javax.sound.midi.*;
+
+/**
+ * User: sanja
+ * Date: 16.07.13
+ * Time: 21:24
+ */
+public class Main {
+
+    public static final int Do = 60;
+
+    public static void main(String[] args) throws MidiUnavailableException {
+        // init sequencer
+        Sequencer sequencer = MidiSystem.getSequencer();
+        sequencer.open();
+
+        // init synthesizer
+        Synthesizer synth = MidiSystem.getSynthesizer();
+        synth.open();
+
+        // get channel for synthesizing: the highest numbered channel.  sets it up
+        MidiChannel[] channels = synth.getChannels();
+        MidiChannel synthChannel = channels[channels.length - 1];
+        synthChannel.programChange(0);
+
+        synthChannel.noteOn(Do, 120);
+
+    }
+}
