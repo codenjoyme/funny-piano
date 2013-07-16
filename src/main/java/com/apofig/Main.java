937f4cb953f238d2d2572189056d3627445ec7cc
Oleksandr Baglai <apofig@gmail.com>
2013-07-16 22:24:23 +0300

    Немного рефакторинга - выделили Синтезатор

--- src/main/java/com/apofig/Main.java
+++ src/main/java/com/apofig/Main.java
@@ -1,7 +1,5 @@
 package com.apofig;
 
-import javax.sound.midi.*;
-
 /**
  * User: sanja
  * Date: 16.07.13
@@ -9,48 +7,29 @@ import javax.sound.midi.*;
  */
 public class Main {
 
-    private static MidiChannel synthChannel;
-
-    public static void main(String[] args) throws MidiUnavailableException, InterruptedException {
-        // init sequencer
-        Sequencer sequencer = MidiSystem.getSequencer();
-        sequencer.open();
-
-        // init synthesizer
-        Synthesizer synth = MidiSystem.getSynthesizer();
-        synth.open();
-
-        // get channel for synthesizing: the highest numbered channel.  sets it up
-        MidiChannel[] channels = synth.getChannels();
-        synthChannel = channels[channels.length - 1];
-        synthChannel.programChange(0);
-        звучать(Тональность.ДоМажор.get(1), 10, 1);
+    public static void main(String[] args) {
+        Синтезатор синтезатор = new Синтезатор();
 
         final int delay = 300;
 
-        звучать(Тональность.ДоМажор.get(1), delay, 120);
-        звучать(Тональность.ДоМажор.get(2), delay, 120);
-        звучать(Тональность.ДоМажор.get(3), delay, 120);
-        звучать(Тональность.ДоМажор.get(4), delay, 120);
-        звучать(Тональность.ДоМажор.get(5), delay, 120);
-        звучать(Тональность.ДоМажор.get(6), delay, 120);
-        звучать(Тональность.ДоМажор.get(7), delay, 120);
-        звучать(Тональность.ДоМажор.get(8), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(1), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(2), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(3), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(4), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(5), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(6), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(7), delay, 120);
+        синтезатор.звучать(Тональность.ДоМажор.get(8), delay, 120);
 
-        звучать(Тональность.ЛяМинор.get(1), delay, 120);
-        звучать(Тональность.ЛяМинор.get(2), delay, 120);
-        звучать(Тональность.ЛяМинор.get(3), delay, 120);
-        звучать(Тональность.ЛяМинор.get(4), delay, 120);
-        звучать(Тональность.ЛяМинор.get(5), delay, 120);
-        звучать(Тональность.ЛяМинор.get(6), delay, 120);
-        звучать(Тональность.ЛяМинор.get(7), delay, 120);
-        звучать(Тональность.ЛяМинор.get(8), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(1), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(2), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(3), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(4), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(5), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(6), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(7), delay, 120);
+        синтезатор.звучать(Тональность.ЛяМинор.get(8), delay, 120);
     }
 
-    private static void звучать(int нота, int длительность, int сила) throws InterruptedException {
-        synthChannel.noteOn(нота, сила);
-        Thread.sleep(длительность);
-        synthChannel.noteOff(нота);
-        Thread.sleep(длительность);
-    }
+
 }
---
+++ src/main/java/com/apofig/Синтезатор.java
@@ -0,0 +1,49 @@
+package com.apofig;
+
+import javax.sound.midi.*;
+
+/**
+ * User: sanja
+ * Date: 16.07.13
+ * Time: 22:20
+ */
+public class Синтезатор {
+
+    private MidiChannel synthChannel;
+
+    public Синтезатор() {
+        try {
+            // init sequencer
+            Sequencer sequencer = null;
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            // init synthesizer
+            Synthesizer synth = MidiSystem.getSynthesizer();
+            synth.open();
+
+            // get channel for synthesizing: the highest numbered channel.  sets it up
+            MidiChannel[] channels = synth.getChannels();
+            synthChannel = channels[channels.length - 1];
+            synthChannel.programChange(0);
+            звучать(Тональность.ДоМажор.get(1), 10, 1);
+        } catch (MidiUnavailableException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void звучать(int нота, int длительность, int сила) {
+        synthChannel.noteOn(нота, сила);
+        пауза(длительность);
+        synthChannel.noteOff(нота);
+        пауза(длительность);
+    }
+
+    private void пауза(int длительность)  {
+        try {
+            Thread.sleep(длительность);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
