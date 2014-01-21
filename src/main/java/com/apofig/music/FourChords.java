37b714d0ed45e0c6dc8722a03dc153a4bd0b3408
Oleksandr Baglai <apofig@gmail.com>
2014-01-21 04:36:37 +0200

    Так правильнее назвать

--- src/main/java/com/apofig/music/FourChords.java
+++ src/main/java/com/apofig/music/FourChords.java
@@ -17,7 +17,8 @@ public class FourChords {
     public static void main(String[] args) {
         Синтезатор синтезатор = new Синтезатор(new RealMidiChannelFactory());
 
-        Шаблон r = Арпеджио.get3(E5, H4, Cis5, A4);
+        Шаблон r = Арпеджиатор.get3(E5, H4, Cis5, A4);
+//        Шаблон r = Арпеджио.get3(G5, D5, E5, C5);
 
         синтезатор.звучать(r.повтор(10));
     }
