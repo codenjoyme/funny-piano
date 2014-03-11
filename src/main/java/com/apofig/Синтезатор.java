4988758a8f0cf27358dbcc016f244c0834a4b11d
Sanja <apofig@gmail.com>
2014-03-11 08:55:37 +0200

    Вроде как наваял немного супермарио, но последнее не очень - потом исправлю

--- src/main/java/com/apofig/Синтезатор.java
+++ src/main/java/com/apofig/Синтезатор.java
@@ -10,7 +10,7 @@ import java.util.*;
  */
 public class Синтезатор {
 
-    public static final int LENGTH = 2000;
+    public static final int LENGTH = 3500;
     private MidiChannel midi;
 
     public Синтезатор(MidiChannelFactory midiFactory) {
